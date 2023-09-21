58b86b555ff263ce495b6ca26f5860c6ebcdc7fe
foreveronlol <koval_fol@yahoo.com>
2023-09-22 01:36:52 +0300

    Change config

--- coin_app/config.py
+++ coin_app/config.py
@@ -7,9 +7,3 @@ postgres_user = os.environ.get('POSTGRES_USER')
 postgres_password = os.environ.get('POSTGRES_PASSWORD')
 postgres_host = os.environ.get('POSTGRES_HOST')
 postgres_db = os.environ.get('POSTGRES_DB')
-
-
-postgres_test_user = os.environ.get('POSTGRES_TEST_USER')
-postgres_test_password = os.environ.get('POSTGRES_TEST_PASSWORD')
-postgres_test_host = os.environ.get('POSTGRES_TEST_HOST')
-postgres_test_db = os.environ.get('POSTGRES_TEST_DB')
